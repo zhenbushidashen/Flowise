f66d4608e46734f7279e2d131f487a68e6fa597b
QUANTAEYE\yuchen <zheniscool@163.com>
2024-06-11 17:26:59 +0800

    feat: brand -> 替换logo

--- packages/ui/src/ui-component/extended/Logo.jsx
+++ packages/ui/src/ui-component/extended/Logo.jsx
@@ -1,5 +1,28 @@
-import logo from '@/assets/images/flowise_logo.png'
-import logoDark from '@/assets/images/flowise_logo_dark.png'
+// import logo from '@/assets/images/flowise_logo.png'
+// import logoDark from '@/assets/images/flowise_logo_dark.png'
+
+
+// import { useSelector } from 'react-redux'
+
+// // ==============================|| LOGO ||============================== //
+
+// const Logo = () => {
+//     const customization = useSelector((state) => state.customization)
+
+//     return (
+//         <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'row' }}>
+//             <img
+//                 style={{ objectFit: 'contain', height: 'auto', width: 150 }}
+//                 src={customization.isDarkMode ? logoDark : logo}
+//                 alt='Flowise'
+//             />
+//         </div>
+//     )
+// }
+
+// export default Logo
+import shuxiLogo from '@/assets/images/shuxi_dark.png'
+import shuxiLogoDark from '@/assets/images/shuxi_logo_black.svg'
 
 import { useSelector } from 'react-redux'
 
@@ -11,12 +34,24 @@ const Logo = () => {
     return (
         <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'row' }}>
             <img
-                style={{ objectFit: 'contain', height: 'auto', width: 150 }}
+                style={{
+                    objectFit: 'contain',
+                    height: 'auto',
+                    width: 100,
+                    filter: customization.isDarkMode ? 'invert(100%)' : 'none',
+                    color: 'transparent'
+                }}
+                src={customization.isDarkMode ? shuxiLogoDark : shuxiLogo}
+                alt='Shuxi'
+            />
+            {/* <img
+                style={{ objectFit: 'contain', height: 'auto', marginLeft: 10, width: 70 }}
                 src={customization.isDarkMode ? logoDark : logo}
                 alt='Flowise'
-            />
+            /> */}
         </div>
     )
 }
 
 export default Logo
+
